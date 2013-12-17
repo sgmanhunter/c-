d1f541b6285b5aded4f3e1678899f3b77bb7569a
KD1993 <kenny.deblaere@live.be>
2013-12-17 16:41:23 +0100

    Rapporten
    Deze werken, je kan alle evenementen opvragen.
    Nu nog enkel in hun juiste vorm weergeven.
    
    Ik kijk daar morgen voor

--- ProjectGroep01/ProjectGroep01/View/ReportForm.Designer.cs
+++ ProjectGroep01/ProjectGroep01/View/ReportForm.Designer.cs
@@ -31,18 +31,44 @@
             this.components = new System.ComponentModel.Container();
             Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
             this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.cSGroep01DataSet = new ProjectGroep01.Data.Databank.CSGroep01DataSet();
+            this.cSGroep01DataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.eventsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.eventsTableAdapter = new ProjectGroep01.Data.Databank.CSGroep01DataSetTableAdapters.eventsTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.cSGroep01DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cSGroep01DataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventsBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // reportViewer1
             // 
-            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
             reportDataSource1.Name = "csGroep01DataSet";
+            reportDataSource1.Value = this.eventsBindingSource;
             this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
             this.reportViewer1.LocalReport.ReportEmbeddedResource = "ProjectGroep01.Data.Databank.Report1.rdlc";
-            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Location = new System.Drawing.Point(1, 5);
             this.reportViewer1.Name = "reportViewer1";
-            this.reportViewer1.Size = new System.Drawing.Size(815, 452);
+            this.reportViewer1.Size = new System.Drawing.Size(816, 451);
             this.reportViewer1.TabIndex = 0;
+            // 
+            // cSGroep01DataSet
+            // 
+            this.cSGroep01DataSet.DataSetName = "CSGroep01DataSet";
+            this.cSGroep01DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // cSGroep01DataSetBindingSource
+            // 
+            this.cSGroep01DataSetBindingSource.DataSource = this.cSGroep01DataSet;
+            this.cSGroep01DataSetBindingSource.Position = 0;
+            // 
+            // eventsBindingSource
+            // 
+            this.eventsBindingSource.DataMember = "events";
+            this.eventsBindingSource.DataSource = this.cSGroep01DataSetBindingSource;
+            // 
+            // eventsTableAdapter
+            // 
+            this.eventsTableAdapter.ClearBeforeFill = true;
             // 
             // ReportForm
             // 
@@ -53,6 +79,9 @@
             this.Name = "ReportForm";
             this.Text = "Rapporten";
             this.Load += new System.EventHandler(this.ReportForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cSGroep01DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cSGroep01DataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventsBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -60,5 +89,10 @@
         #endregion
 
         private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private Data.Databank.CSGroep01DataSet cSGroep01DataSet;
+        private System.Windows.Forms.BindingSource cSGroep01DataSetBindingSource;
+        private System.Windows.Forms.BindingSource eventsBindingSource;
+        private Data.Databank.CSGroep01DataSetTableAdapters.eventsTableAdapter eventsTableAdapter;
+
     }
 }
---
+++ ProjectGroep01/ProjectGroep01/View/ReportForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ProjectGroep01.Data.Databank;
+using ProjectGroep01.Data.Databank.CSGroep01DataSetTableAdapters;
+
+namespace ProjectGroep01.View
+{
+    public partial class ReportForm : Form, IObserver
+    {
+        public ReportForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ReportForm_Load(object sender, EventArgs e)
+        {
+            FillReport();
+        }
+
+        private void FillReport()
+        {
+            // TODO: This line of code loads data into the 'cSGroep01DataSet.events' table. You can move, or remove it, as needed.
+            this.eventsTableAdapter.Fill(this.cSGroep01DataSet.events);
+
+            this.reportViewer1.RefreshReport();
+        }
+
+        public void Updating()
+        {
+            FillReport();
+        }
+    }
+}
